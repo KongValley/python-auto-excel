4c24504b25be3833229a3f4e11a8bd9604de188d
Chara <chara1997@foxmail.com>
2020-02-29 18:27:49 +0800

    Add: 添加os练习代码

--- openpyxl-module.py
+++ openpyxl-module.py
@@ -1,2 +1,7 @@
-import openpyxl as tool
+#!/usr/bin/python
+# @Author: Chara
+# @Description:
+# @Time: 2020/2/28 15:50
+
+from openpyxl import load_workbook as ld
 
--- os-module.py
+++ os-module.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+# @Author: Chara
+# @Description:
+# @Time: 2020/2/28 16:07
+
 import os
 
-print(os.getcwd())
+# rule = ['xls', 'xlsx']
+rule = ['py']
+
+# 获取当前路径
+local = os.getcwd()
+
+# 输出当前路径下符合过滤后缀名的文件
+print([el for el in os.listdir(local) if el.split('.')[1] in rule])
