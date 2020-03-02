9743bd0c55b78fef758019bdf95024da302f1156
Chara <704002390@qq.com>
2020-03-02 16:39:31 +0800

    Add: 更新用户名

--- openpyxl-module.py
+++ openpyxl-module.py
@@ -2,6 +2,19 @@
 # @Author: Chara
 # @Description:
 # @Time: 2020/2/28 15:50
-
+import os
+import pandas as pd
 from openpyxl import load_workbook as ld
+
+# 过滤出 excel 文件
+rule = ['xls', 'xlsx']
+
+# 获取当前路径
+local = os.getcwd()
+
+# 获取当前路径下的 excel 文件列表
+excelArr = [el for el in os.listdir(local) if el.split('.')[1] in rule]
+
+# 通过文件名读入数据
+wbArr = [ld(i) for i in excelArr]
 
