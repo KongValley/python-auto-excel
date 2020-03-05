e570640f9c9bdf23e9153ddaee3e059dc6f71882
Chara <chara1997@foxmail.com>
2020-03-05 19:43:59 +0800

    :pencil: 更新文档和完善异常处理

--- src/pandas-module.py
+++ src/pandas-module.py
@@ -4,13 +4,15 @@
 # @Time: 2020/2/28 15:50
 import os
 import pandas as pd
-from openpyxl import load_workbook as ld
 
 # 过滤出 excel 文件
 rule = ['xls', 'xlsx']
 
 # 获取当前路径
 local = os.getcwd()
+
+# test-1
+# local = os.path.join(os.getcwd(),'test-1')
 
 # 输出路径
 outUrl = os.path.join(local, "output.xlsx")
@@ -22,12 +24,12 @@ if os.path.exists(outUrl):
 # 获取当前路径下的 excel 文件列表
 excelArr = [os.path.join(local, el) for el in os.listdir(local) if el.split('.')[1] in rule]
 
-# 通过文件名读入数据
-# header 的值即为设置标题所在的行数，默认为 0
-wbArr = [pd.read_excel(i, header=1) for i in excelArr]
-
-writer = pd.ExcelWriter(outUrl)
-
-pd.concat(wbArr).to_excel(writer, 'Sheet1', index=False)
-
-writer.save()
+if len(excelArr) == 0:
+    print('无 excel 文件')
+else:
+    # 通过文件名读入数据
+    # header 的值即为设置标题所在的行数，默认为 0
+    wbArr = [pd.read_excel(i, header=1) for i in excelArr]
+    writer = pd.ExcelWriter(outUrl)
+    pd.concat(wbArr).to_excel(writer, 'Sheet1', index=False)
+    writer.save()
